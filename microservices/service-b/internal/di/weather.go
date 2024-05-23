c52bd0d4e19f75fe5dac486149db2883223f986f
Leonardo Rangel <37275024+leo12wb@users.noreply.github.com>
2024-05-23 09:27:56 -0300

    Update weather.go

--- microservices/service-b/internal/di/weather.go
+++ microservices/service-b/internal/di/weather.go
@@ -3,11 +3,11 @@ package di
 import (
 	"net/http"
 
-	"github.com/booscaaa/desafio-sistema-de-temperatura-por-cep-otel-go-expert-pos/microservices/service-b/internal/entity"
-	"github.com/booscaaa/desafio-sistema-de-temperatura-por-cep-otel-go-expert-pos/microservices/service-b/internal/infra/viacep"
-	"github.com/booscaaa/desafio-sistema-de-temperatura-por-cep-otel-go-expert-pos/microservices/service-b/internal/infra/weatherapi"
-	"github.com/booscaaa/desafio-sistema-de-temperatura-por-cep-otel-go-expert-pos/microservices/service-b/internal/infra/web"
-	"github.com/booscaaa/desafio-sistema-de-temperatura-por-cep-otel-go-expert-pos/microservices/service-b/internal/usecase"
+	"github.com/leo12wb/Tracing-distribu-do-e-span/microservices/service-b/internal/entity"
+	"github.com/leo12wb/Tracing-distribu-do-e-span/microservices/service-b/internal/infra/viacep"
+	"github.com/leo12wb/Tracing-distribu-do-e-span/microservices/service-b/internal/infra/weatherapi"
+	"github.com/leo12wb/Tracing-distribu-do-e-span/microservices/service-b/internal/infra/web"
+	"github.com/leo12wb/Tracing-distribu-do-e-span/microservices/service-b/internal/usecase"
 	"github.com/go-playground/validator/v10"
 )
 
