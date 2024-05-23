2dc6e790b02afb6777b186f2050751efd02f33ae
Leonardo Rangel <37275024+leo12wb@users.noreply.github.com>
2024-05-23 13:55:56 -0300

    Update cep.go

--- microservices/service-b/internal/infra/viacep/cep.go
+++ microservices/service-b/internal/infra/viacep/cep.go
@@ -36,26 +36,19 @@ func (httpclient httpclient) Get(ctx context.Context, cep string) (*entity.Cep, 
 	url := fmt.Sprintf("%s/ws/%s/json/", BASE_URL, cep)
 
 	request, err := http.NewRequestWithContext(ctx, http.MethodGet, url, bytes.NewReader(nil))
-
 	if err != nil {
-
 		return nil, err
 	}
-
 	defer request.Body.Close()
 
 	response, err := httpclient.client.Do(request)
-
 	if err != nil {
-
 		return nil, err
 	}
-
 	defer response.Body.Close()
 
-	if response.StatusCode == 200 {
+	if response.StatusCode == http.StatusOK {
 		err = json.NewDecoder(response.Body).Decode(&cepOutput)
-
 		if err != nil {
 			return nil, err
 		}
@@ -70,5 +63,65 @@ func (httpclient httpclient) Get(ctx context.Context, cep string) (*entity.Cep, 
 		}, nil
 	}
 
+	if response.StatusCode == http.StatusNotFound {
+		// Cria um objeto de erro
+		errorResponse := map[string]string{
+			"error": "404 can no find zipcode.",
+		}
+		// Serializa o objeto de erro em JSON
+		errorJSON, err := json.Marshal(errorResponse)
+		if err != nil {
+			return nil, err
+		}
+		// Retorna o erro em formato JSON
+		return nil, fmt.Errorf(string(errorJSON))
+	}
+
 	return nil, errorhandle.ErrUnprocessableEntity
 }
+
+// func (httpclient httpclient) Get(ctx context.Context, cep string) (*entity.Cep, error) {
+// 	tr := otel.Tracer("microservice-trace")
+// 	ctx, span := tr.Start(ctx, "get cep")
+// 	defer span.End()
+
+// 	var cepOutput dto.CepOutput
+// 	url := fmt.Sprintf("%s/ws/%s/json/", BASE_URL, cep)
+
+// 	request, err := http.NewRequestWithContext(ctx, http.MethodGet, url, bytes.NewReader(nil))
+
+// 	if err != nil {
+
+// 		return nil, err
+// 	}
+
+// 	defer request.Body.Close()
+
+// 	response, err := httpclient.client.Do(request)
+
+// 	if err != nil {
+
+// 		return nil, err
+// 	}
+
+// 	defer response.Body.Close()
+
+// 	if response.StatusCode == 200 {
+// 		err = json.NewDecoder(response.Body).Decode(&cepOutput)
+
+// 		if err != nil {
+// 			return nil, err
+// 		}
+
+// 		if cepOutput.Erro == "true" {
+// 			return nil, errorhandle.ErrNotFound
+// 		}
+
+// 		return &entity.Cep{
+// 			Cep:      cepOutput.Cep,
+// 			CityName: cepOutput.Localidade,
+// 		}, nil
+// 	}
+
+// 	return nil, errorhandle.ErrUnprocessableEntity
+// }
