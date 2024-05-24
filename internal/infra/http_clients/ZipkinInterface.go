33060ad25a15222f9c54619deaed0ae0b98e9382
leo12wb <leonardo502013@gmail.com>
2024-05-24 10:33:01 -0300

    ajuste

--- internal/infra/http_clients/ZipkinInterface.go
+++
@@ -1,7 +0,0 @@
-package http_clients
-
-import "net/http"
-
-type ZipkinClientInterface interface {
-	DoWithAppSpan(req *http.Request, span string) (*http.Response, error)
-}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/leo12wb/Tracing-distribu-do-e-span/cmd/go_expert_zipkin"
-
-func main() {
-	go_expert_zipkin.Bootstap()
-}
